62dcb6c08d1d2ac47169d28d9ff707941e9fdad2
Vlist <yuanhaoxiang2000@outlook.com>
2021-06-15 02:24:33 +0800

    Add Drawer Layout and Toolbar

--- app/src/main/java/com/vlist/holaenhanced/Chat/ChatViewHolders.java
+++ app/src/main/java/com/vlist/holaenhanced/Chat/ChatViewHolders.java
@@ -3,21 +3,26 @@ package com.vlist.holaenhanced.Chat;
 
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.vlist.holaenhanced.R;
 
-public class ChatViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class ChatViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
     public TextView mMessage;
-    public LinearLayout mContainer;
+    public CardView mCard;
+    public RelativeLayout mContainer;
+
     public ChatViewHolders(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
 
-        mMessage = itemView.findViewById(R.id.message);
-        mContainer = itemView.findViewById(R.id.container);
+        mMessage = itemView.findViewById(R.id.text_message);
+        mCard = itemView.findViewById(R.id.message_container_card);
+        mContainer = itemView.findViewById(R.id.message_container);
     }
 
     @Override
